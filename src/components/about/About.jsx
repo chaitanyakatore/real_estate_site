40af9e8c1ef2dc96aa3c895b7d2188a7e11794ef
prashantkharche <prashantkharche2002@gmail.com>
2023-11-15 13:29:05 +0530

    Top loader added

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,12 +1,29 @@
-import React from "react"
+import React, {useState, useEffect} from "react"
 import Back from "../common/Back"
 import Heading from "../common/Heading"
 import img from "../images/about.jpg"
 import "./about.css"
 
+import LoadingBar from 'react-top-loading-bar'
+
 const About = () => {
+  const [progress, setProgress] = useState(0)
+  useEffect(()=>{
+    setProgress(70);
+    setTimeout(()=> {
+      setProgress(100);
+    }, 1000 )
+    
+  },[])
   return (
     <>
+      <LoadingBar
+        color='#27ae60'
+        progress={progress}
+        onLoaderFinished={() => setProgress(0)}
+      />
+
+      
       <section className='about'>
         <Back name='About Us' title='About Us - Who We Are?' cover={img} />
         <div className='container flex mtop'>
--- src/components/services/Services.jsx
+++ src/components/services/Services.jsx
@@ -1,12 +1,29 @@
-import React from "react"
+import React, {useState, useEffect} from "react"
 import img from "../images/services.jpg"
 import Back from "../common/Back"
 import "../home/featured/Featured.css"
 import FeaturedCard from "../home/featured/FeaturedCard"
 
+import LoadingBar from 'react-top-loading-bar'
+
 const Services = () => {
+  const [progress, setProgress] = useState(0)
+  useEffect(()=>{
+    setProgress(70);
+    setTimeout(()=> {
+      setProgress(100);
+    }, 1000 )
+    
+  },[])
+
   return (
     <>
+      <LoadingBar
+        color='#27ae60'
+        progress={progress}
+        onLoaderFinished={() => setProgress(0)}
+      />
+
       <section className='services mb'>
         <Back name='Services' title='Services -All Services' cover={img} />
         <div className='featured container'>
